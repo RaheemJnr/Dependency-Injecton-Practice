00aa8bb90452e3f8444926b3990c88161416bd8a
Jnr <mumedian6@gmail.com>
2022-05-06 12:14:42 +0100

    first commit for DI practice

--- app/src/main/java/com/raywenderlich/android/busso/di/ServiceLocatorImpl.kt
+++ app/src/main/java/com/raywenderlich/android/busso/di/ServiceLocatorImpl.kt
@@ -3,12 +3,14 @@ package com.raywenderlich.android.busso.di
 import android.annotation.SuppressLint
 import android.content.Context
 import android.location.LocationManager
+import com.raywenderlich.android.busso.network.provideBussoEndPoint
 import com.raywenderlich.android.busso.permission.GeoLocationPermissionCheckerImpl
 import com.raywenderlich.android.location.rx.provideRxLocationObservable
 
 const val LOCATION_OBSERVABLES = "LocationObservables"
 
 const val ACTIVITY_LOCATOR_FACTORY = "ActivityLocationFactory"
+const val BUSSO_ENDPOINT = "BussoEndpoint"
 
 
 class ServiceLocatorImpl(val context: Context) : ServiceLocator {
@@ -20,10 +22,17 @@ class ServiceLocatorImpl(val context: Context) : ServiceLocator {
     private val locationObservables =
         provideRxLocationObservable(locationManager, geoLocationPermissionChecker)
 
-    @Suppress("IMPLICIT_CAST_TO_ANY","UNCHECKED_CAST")
+    //
+    private val bussoEndpoint = provideBussoEndPoint(context)
+
+    @Suppress("IMPLICIT_CAST_TO_ANY", "UNCHECKED_CAST")
     @SuppressLint("ServiceCast")
     override fun <A : Any> lookUp(name: String): A = when (name) {
+        //
         LOCATION_OBSERVABLES -> locationObservables
+        //
+        BUSSO_ENDPOINT -> bussoEndpoint
+        //
         ACTIVITY_LOCATOR_FACTORY -> activityServiceLocatorFactory(this)
         else -> throw
         IllegalArgumentException("No component lookup for the key: $name")
